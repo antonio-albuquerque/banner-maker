7a129e16109138f6757b23afe2ef63480baac1c8
Antonio Carlos Albuquerque Junior <antonio.junior@dft-sp-wkn784.dafiti.local>
2020-06-29 08:15:39 -0300

    some adjusts

--- scripts.js
+++ scripts.js
@@ -8,9 +8,11 @@ const draw = (label, price) => {
   ctx.font = '48px arial';
   ctx.fillStyle = 'white';
   ctx.textAlign = 'center';
-  ctx.fillText(label, 360, 80);
-  ctx.font = '150px serif';
-  ctx.fillText(price, 360, 350);
+  ctx.fillText(label, 160, 140);
+  ctx.font = '32px arial';
+  ctx.fillText('R$', 50, 280);
+  ctx.font = '100px serif';
+  ctx.fillText(price, 160, 350);
   make_base(ctx);
   console.log(ctx);
 };
